3474995fa3e0ccc5f96c8453d2a53db85eccc350
timo.schlumpf <70700633+Wadafacc@users.noreply.github.com>
2022-08-31 23:27:12 +0200

    fix z-index please, thanks

---
+++ components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="header">
+    <img src="~/static/logo.png" alt="" />
+
+    <div class="nav-btn" @click="anim()">
+      <div class="center" id="nav"></div>
+      <div class="nav" id="menu">
+        <div class="text">
+          <a href="">who am i?</a>
+          <a href="">what have i done?</a>
+          <a href="">where did i learn that?</a>
+          <a href="">wanna go for a coffee?</a>
+        </div>
+        <div class="bg"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped src="~/styles/header.css">
+</style>
+
+<script>
+export default {
+  methods: {
+    anim() {
+      let element = document.getElementById("nav");
+      let menu = document.getElementById("menu");
+      if (!element.classList.contains("anim")) {
+        element.classList.add("anim");
+        menu.style.display = "block";
+      } else {
+        element.classList.remove("anim");
+        menu.style.display = "none";
+      }
+    },
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="main">
+    <div class="header">
+      <Header />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
