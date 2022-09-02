ff9fda8f62a38fd5f3f5a043b19259794a222f68
wadafacc <schlumpftimo14@gmail.com>
2022-09-02 16:18:03 +0200

    awd

--- components/Header.vue
+++ components/Header.vue
@@ -2,16 +2,15 @@
   <div class="header">
     <img src="~/static/logo.png" alt="" />
 
-    <div class="nav-btn" @click="anim()">
-      <div class="center" id="nav"></div>
+    <div class="navigation">
+      <div class="btn" id="nav" @click="anim()"></div>
       <div class="nav" id="menu">
-        <div class="text">
+        <div class="text" id="text">
           <a href="">who am i?</a>
           <a href="">what have i done?</a>
           <a href="">where did i learn that?</a>
           <a href="">wanna go for a coffee?</a>
         </div>
-        <div class="bg"></div>
       </div>
     </div>
   </div>
@@ -26,12 +25,15 @@ export default {
     anim() {
       let element = document.getElementById("nav");
       let menu = document.getElementById("menu");
+      let txt = document.getElementById("text");
       if (!element.classList.contains("anim")) {
         element.classList.add("anim");
-        menu.style.display = "block";
+        menu.style.height = "100%";
+        txt.style.height = "100%";
       } else {
         element.classList.remove("anim");
-        menu.style.display = "none";
+        menu.style.height = "0%";
+        txt.style.height = "0%";
       }
     },
   },
